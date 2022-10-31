1b2baada87f826defb0bfb28f7c8e8742d3d0b39
Grigorij Stafeev <Grigorij Stafeev>
2022-10-31 13:15:28 +0000

    exceptions

--- huflib/archiver.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include <string>
-
-void encode(std::istream&, std::ostream&);
-void decode(std::istream&, std::ostream&);
--- huflib/huffman_exception.cpp
+++
@@ -1,7 +0,0 @@
-//
-// Created by staf02 on 31/10/22.
-//
-
-#include "huffman_exception.h"
-
-huffman_exception::huffman_exception(const char *message) : runtime_error(message) {}
